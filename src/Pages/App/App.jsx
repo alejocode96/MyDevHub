b1a195a68b613afc01e7b67d8914573ab54fbfdf
Diego Alejandro Galeano Madrigal <dgaleano28@argos.com.co>
2025-12-23 21:32:30 -0500

    chore: initialize React project with Vite

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-400'>
+       hola proyecto base creado
+      </div>
+     
+    </>
+  )
+}
+
+export default App
