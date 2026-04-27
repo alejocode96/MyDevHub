9f28523a861b66ac597a31325f5c7fccdda377f5
Alejandro Galeano <alejocode96@email.com>
2026-04-27 15:31:26 -0500

    feat: Add HeroSection

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 
 //componentes
-import NavBar from '../../components/Home/navbar'
+import NavBar from '../../components/Home/navbar';
+import HeroSection from '../../components/Home/heroSection';
 const Home = () => {
     return (
         <>
             {/*Navbar - Menu */}
             <NavBar></NavBar>
+            <main className='mb-20'>
+                <HeroSection />
+            </main>
         </>
     )
 }
